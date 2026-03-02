58fb178bd5ee0c3616db0e7c2d4b4a80c330594c
Dein <deine.email@example.com>
2026-03-02 09:15:54 +0100

    -feat: added FileWriter logic to write report to a csv file

--- src/main/java/core/basesyntax/fileservice/writer/CsvWriterImpl.java
+++ src/main/java/core/basesyntax/fileservice/writer/CsvWriterImpl.java
@@ -1,8 +1,22 @@
 package core.basesyntax.fileservice.writer;
 
+import core.basesyntax.fileservice.report.ReportGenerator;
+import core.basesyntax.fileservice.report.ReportGeneratorImpl;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class CsvWriterImpl implements FileWriter {
     @Override
     public void writeTo(String fromString, String toFilePath) {
-
+        File csvOutputFile = new File(toFilePath);
+        ReportGenerator reportGenerator = new ReportGeneratorImpl();
+        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+            pw.write(reportGenerator.getReport());
+        } catch (IOException e) {
+            return;
+        }
     }
 }
